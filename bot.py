b61d5489058688d8b66bfb0b639872fa4ca1c888
mashinabozorda-droid <mashinabozorda@gmail.com>
2026-01-06 23:06:21 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,16 @@
+import telebot
+import os
 
+TOKEN = os.getenv("6673316226:AAFqXnQqvz6pXegT8VLMQ3axck0SFN40RZ4")
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(
+        message.chat.id,
+        f"Salom {message.from_user.first_name}! Bot ishlayapti ✅"
+    )
+
+print("Bot ishga tushdi...")
+
+bot.infinity_polling()
